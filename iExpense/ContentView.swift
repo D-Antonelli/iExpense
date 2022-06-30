a3560e610d9e5e2cf498702904d930d84038a1e6
derya <derya@boonandmoil.dev>
2022-06-30 11:43:21 +0100

    refactors code structure

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,53 +7,7 @@
 
 import SwiftUI
 
-struct ListRowView: View {
-    
-    
-    var item: ExpenseItem
-    
-    var body: some View {
-        Section(header: Text(item.type)) {
-            HStack {
-                VStack(alignment: .leading) {
-                    Text(item.name)
-                        .font(.headline)
-                    Text(item.type)
-                }
-                
-                Spacer()
-                
-                Text(item.amount, format: .currency(code: "USD"))
-                
-                    .foregroundColor(Color.white)
-                    .listRowBackground(getColor(for: item.amount))
-            }
-            
-        }
-        
-    }
-    
-    func getColor(for amount: Double) -> some View {
-        if(amount < 50.00) {
-            return Color.green
-        }
-        else if(amount < 500.00) {
-            return Color.yellow
-        }
-        
-        else if(amount < 1000.00) {
-            return Color.orange
-        }
-        
-        else if(amount < 10000.00) {
-            return Color.purple
-        }
-        else if(amount >= 10000.00) {
-            return Color.red
-        }
-        return Color.clear
-    }
-}
+
 
 
 struct ContentView: View {
@@ -67,7 +21,7 @@ struct ContentView: View {
     var personalExpenses: [Binding<ExpenseItem>] {
         $expenses.items.filter { $0.type.wrappedValue == "Personal"}
     }
-
+    
     
     var body: some View {
         NavigationView {
@@ -76,34 +30,26 @@ struct ContentView: View {
                     Section(header: Text(section.rawValue)) {
                         let filteredExpenses = section == .business ? businessExpenses : personalExpenses
                         
+                        if filteredExpenses.isEmpty {
+                            Text("No expenses available")
+                        }
+                        
                         ForEach(filteredExpenses) { $item in
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text(item.name)
-                                        .font(.headline)
-                                    Text(item.type)
-                                }
-                                
-                                Spacer()
-                                
-                                Text(item.amount, format: .currency(code: "USD"))
-                                    .foregroundColor(Color.white)
-                                    .listRowBackground(getColor(for: item.amount))
-                            }
+                            ExpenseViewCell(item: $item)
                         }
                         .onDelete {indexSet in
                             indexSet.forEach { index in
                                 let expenseToDelete = filteredExpenses[index]
                                 expenses.items = expenses.items.filter { $0.id != expenseToDelete.id }
                             }
-
+                            
+                            
+                        }
+                        
                         
                     }
-                   
-                
+                    
                 }
-                
-            }
             }
             .navigationTitle("iExpense")
             .toolbar {
@@ -118,28 +64,6 @@ struct ContentView: View {
             AddView(expenses: expenses)
         }
         
-    }
-    
-    
-    func getColor(for amount: Double) -> some View {
-        if(amount < 30.00) {
-            return Color.green
-        }
-        else if(amount < 100.00) {
-            return Color.yellow
-        }
-        
-        else if(amount < 300) {
-            return Color.orange
-        }
-        
-        else if(amount < 500) {
-            return Color.purple
-        }
-        else if(amount >= 500) {
-            return Color.red
-        }
-        return Color.clear
     }
     
 }
---
+++ iExpense/ExpenseViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  ExpenseViewCell.swift
+//  iExpense
+//
+//  Created by Derya Antonelli on 30/06/2022.
+//
+
+import SwiftUI
+
+struct ExpenseViewCell: View {
+    
+    @Binding var item: ExpenseItem
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(item.name)
+                    .font(.headline)
+                Text(item.type)
+            }
+            
+            Spacer()
+            
+            Text(item.amount, format: .currency(code: "USD"))
+        }
+        .listRowBackground(getColor(for: item.amount))
+    }
+    
+    func getColor(for amount: Double) -> some View {
+        if(amount < 50.00) {
+            return Color.green
+        }
+        else if(amount < 500.00) {
+            return Color.yellow
+        }
+        
+        else if(amount < 1000.00) {
+            return Color.orange
+        }
+        
+        else if(amount < 10000.00) {
+            return Color.purple
+        }
+        else if(amount >= 10000.00) {
+            return Color.red
+        }
+        return Color.clear
+    }
+}
+
+
+struct ExpenseViewCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ExpenseViewCell(item: .constant(ExpenseItem(name: "Grocery", type: Sections.personal.rawValue, amount: 50)))
+    }
+}
